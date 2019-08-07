153d82e37369e28739b1328f64759701537a9658
CJTaylor <tka4inni@gmail.com>
2019-08-07 15:29:01 +0300

    Change: JS exercises.js (template literal/formula/tag function)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,18 +1,13 @@
 'use strict';
 
-(function dom1() {
-	const bd = document.createElement('body');
-	const ht = document.querySelector('html');
-	const btn = document.createElement('button');
-	const cssString = 'display: inline-block; width: 150px; height: 100px; margin: 50px 50px; font-size: 35px; border: 1px solid black; background: lightgrey;';
+(function() {
+	function literals(ltrls, ...expressions) {
+			const a = expressions[0];
+			const p = expressions[1];
+			const s = (p + Math.sqrt((p * p) - (16 * a))) / 4;  
 
-		ht.appendChild(btn);
+			console.log(`Formula value: ${s}`);
+	}
 
-	const butt = document.querySelector('button');
-	butt.style = cssString;
-	butt.innerHTML = 0;
-
-		butt.addEventListener('click', function() {
-			butt.innerHTML++;
-		});
+	literals `Rectangle area: ${140} Rectangle perimeter: ${48}`
 })();
