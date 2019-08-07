38df2ed6c68a2f642b5ecab11256e88c28240b7c
CJTaylor <tka4inni@gmail.com>
2019-08-07 17:14:37 +0300

    Change: JS exercises.js (numbers rotate around 5 pure JS - messy verion)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,13 +1,43 @@
 'use strict';
 
-(function() {
-	function literals(ltrls, ...expressions) {
-			const a = expressions[0];
-			const p = expressions[1];
-			const s = (p + Math.sqrt((p * p) - (16 * a))) / 4;  
+(function dom2() {
 
-			console.log(`Formula value: ${s}`);
-	}
+	const cssStringDiv = 'display: grid; grid-template-rows: repeat(3, 1fr); grid-template-columns: repeat(3, 1fr); grid-gap: 5px; width: 400px; height: 400px; margin: 20px;';
+	const cssStringBut = 'font-size: 30px;';
+	const arrNumb = ['+3','-1','-1','+3','0','-3','+1','+1','-3'];
 
-	literals `Rectangle area: ${140} Rectangle perimeter: ${48}`
+		(function mainDIv() {	// create main div container
+			const div = document.createElement('div');
+			document.querySelector('html').appendChild(div);
+			div.id = 'btns';
+			div.style = cssStringDiv;
+		})();
+
+		(function childDivs() {		// create main div child buttons
+			let btn;
+				for (let i = 0; i < 9; i++) {
+					btn = document.createElement('button');
+					document.querySelector('#btns').appendChild(btn);
+					btn.id = `btn${i + 1}`;
+					btn.setAttribute('data-rotat',arrNumb[i]);
+					btn.innerHTML = i + 1;
+					btn.style = cssStringBut;
+				}
+		})();
+
+		(function event() {
+			const five = document.querySelector('#btn5');
+			five.addEventListener('click', rotation);
+
+				function rotation() {
+					const allBtn = document.querySelectorAll('button');
+					allBtn.forEach(function(i) {
+						let x = i.innerHTML;
+						let z = i.getAttribute('data-rotat');
+						let w = +x + +z;
+						i.innerHTML = w; 
+						i.setAttribute('data-rotat', arrNumb[w - 1]);	
+					});
+				}
+		})();
 })();
