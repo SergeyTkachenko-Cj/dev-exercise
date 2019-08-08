38ebb9be9e04a6b7df702d3dabd300ba3def8d3a
CJTaylor <tka4inni@gmail.com>
2019-08-08 15:14:48 +0300

    Change: JS exercises.js (arrow function/default param/rest/spread)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,41 +1,40 @@
 'use strict';
 
-(function dom2() {
+(() => {	// arrow functions
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 
-	const cssStringDiv = 'display: grid; grid-template-rows: repeat(3, 1fr); grid-template-columns: repeat(3, 1fr); grid-gap: 5px; width: 400px; height: 400px; margin: 20px;';
-	const cssStringBut = 'font-size: 30px; pointer-events: none;';
-	const cssStringFive = 'outline: none; pointer-events: auto; color: #fff; background: tomato;';
-	const arrNum = [1, 2, 3, 6, 9, 8, 7, 4];
-	const idsNum = [1, 2, 3, 6, 9, 8, 7, 4];
+const squareList = ((arr) => {
+  const squaredIntegers = arr.map((i) => { if (Number.isInteger(i) && i > 0) return i * i });
+  console.log(squaredIntegers.filter((i) => i));
+})(realNumberArray);
+})();
 
-		(function mainDIv() {	// create main div container
-			const div = document.createElement('div');
-			document.querySelector('html').appendChild(div);
-			div.id = 'btns';
-			div.style = cssStringDiv;
-		})();
 
-		(function childDivs() {		// create main div child buttons
-				for (let i = 0; i < 9; i++) {
-					var btn = document.createElement('button');
-					btn.id = `btn${i + 1}`;
-					btn.innerHTML = i + 1;
-					btn.style = cssStringBut;
-					document.querySelector('#btns').appendChild(btn);
-				}
-			document.querySelector('#btn5').style.cssText += cssStringFive;
-		})();
+(() => {	// default parameters
+const increment = () => {
+  return (number, value = 1) => console.log(number + value)
+};
+increment()(5,2);
+increment()(5);
+})();
 
-		(function event() {		// rotation event
-			const five = document.querySelector('#btn5');
-			const allBtnNoFive = document.querySelectorAll('#btns > button:not(#btn5)');
-			five.addEventListener('click', rotation);
 
-				function rotation() {
-					arrNum.unshift(arrNum.pop());
-					allBtnNoFive.forEach((i, j) => document.querySelector(`#btn${idsNum[j]}`).innerHTML = arrNum[j]);
-				}
-		})();
+(() => {	// rest parameter
+const sum = (() => (...args) => args.reduce((a, b) => a + b, 0))();
+console.log(sum(1, 2, 3)); // 6
+})();
+
+
+
+(() => {	// spread operator
+	const obj1 = { java : 'script', command : 'line' };
+	const arr1 = ['java','script','command','line'];
+
+	const obj2 = {...obj1};
+	const arr2 = [...arr1];
+
+	console.log(obj2);
+	console.log(arr2);
 })();
 
 
