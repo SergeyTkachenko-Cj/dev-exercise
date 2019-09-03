0f52d8128bc61bf9bb76ac29a8d02ace91d86aad
CJTaylor <tka4inni@gmail.com>
2019-09-03 09:34:59 +0300

    New: JS exercises.js (promises with timeout)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,25 +1,24 @@
 
-const doggoIsAGoodBoy = false;
+const serverImitation = (response) => {
+  const trueOrFalse = response;
 
-const willGetABone = new Promise((resolve, reject) => {
-  if (doggoIsAGoodBoy) {
-    const bone = {
-      meat : 'angus',
-      size : 'huge'
-    };
+  const serverResponse = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const serverPermission = trueOrFalse;
+      
+      if (serverPermission) resolve('permission granted');
+      else {
+        const err = new Error('permission denied');
+        reject(err);
+      }
+    }, 1000);
+  });
 
-    resolve(`gets the bone: meat - ${bone.meat}, size - ${bone.size}`);
-  }
-  else {
-    const why = 'pooped on the floor';
-    reject(`gets no bone: ${why}`);
-  }
-});
+  const serverCall = () => {
+    serverResponse.then((good) => console.log(good)).catch((bad) => console.log(bad));
+  };
 
-const decision = () => {
-  willGetABone
-  .then(approved => console.log(approved))
-  .catch(rejected => console.log(rejected));
+  serverCall();
 };
 
-decision();
+serverImitation(true);
