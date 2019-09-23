a82b19f2874167f5b81233b5f52bd5cf82b24514
CJTaylor <tka4inni@gmail.com>
2019-09-23 15:58:06 +0300

    JS exercises.js (multiple promises with API fetch)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,24 +1,27 @@
 
-const serverImitation = (response) => {
-  const trueOrFalse = response;
+const multiPromises = (() => {
+  const states = [
+    'new_york',
+    'california',
+    'ohio',
+    'illinois',
+  ];
 
-  const serverResponse = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const serverPermission = trueOrFalse;
-      
-      if (serverPermission) resolve('permission granted');
-      else {
-        const err = new Error('permission denied');
-        reject(err);
-      }
-    }, 1000);
-  });
+  const concat = [];
 
-  const serverCall = () => {
-    serverResponse.then((good) => console.log(good)).catch((bad) => console.log(bad));
-  };
+  const responses = states.map(
+    (item) => fetch(`https://api.openbrewerydb.org/breweries?per_page=1&by_state=${item}`),
+  );
 
-  serverCall();
-};
-
-serverImitation(true);
+  Promise.all(responses)
+    .then((resps) => Promise.all(resps.map((item) => item.json())))
+    .then((resps) => resps.map((ite) => ite.map(
+      (it) => console.log(
+        '%c%s%c : %c%s%c (%c%s%c)',
+        'color:orange', it.state,
+        'background:inherit;', 'color:yellow; font-style: italic;', it.name,
+        'background:inherit;', 'color:orange;', it.city,
+        'background:inherit;',
+      ),
+    )));
+})();
