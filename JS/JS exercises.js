fe8c7d278541fbda461cc5bf2c1a38ee587f45e3
CJTaylor <tka4inni@gmail.com>
2019-10-02 10:03:40 +0300

    JS exercises.js (.camelCase method, no spaces, first letter capitalized)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,13 +1,16 @@
 
-function divisors(integer) {
-  const res = [];
-  for (let i = 2; i < integer; i++) {
-    const divide = integer / i;
-    if (!(Math.trunc(divide) - divide)) res.push(divide);
-  }
-  return res.length ? res.reverse() : `${integer} is prime`;
-}
+String.prototype.camelCase = function () {
+  const x = this.split(' ');
+  const z = x.map((i) => {
+    const q = i.split('').map((item, index) => (index ? item : item.toUpperCase()));
+    return q.join('');
+  });
 
-console.log(divisors(12)); // should return [2,3,4,6]
-// console.log(divisors(25)); // should return [5]
-// console.log(divisors(13)); // should return "13 is prime"
+  return z.join('');
+};
+
+console.log('hello case '.camelCase());
+
+
+// "hello case".camelCase() => HelloCase;
+// "camel case word".camelCase() => CamelCaseWord;
