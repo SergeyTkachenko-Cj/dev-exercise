29f8ab1ee23b36ca3ca2a1097b8631f176eb952c
CJTaylor <tka4inni@gmail.com>
2019-10-09 12:08:42 +0300

    JS exercises.js (rice and chessboard problem - quick solution)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,4 +1,15 @@
 
-const multiplyAll = arr => int => arr.map(i => i * int);
+function squaresNeeded(grains) {
+  if (!grains) return 0;
 
-console.log(multiplyAll([1, 2, 3])(2));
+  let count = 1;
+
+  for (let i = 1; i < grains; i++) {
+    i *= 2;
+    count++;
+  }
+
+  return count;
+}
+
+console.log(squaresNeeded(3));
