a7a16d348b0a88f3846405b9c20856aa9b308fe8
CJTaylor <tka4inni@gmail.com>
2019-10-11 14:02:38 +0300

    JS exercises.js (count how often sign changes in array)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,9 +1,8 @@
 
-function calculate(str) { 
-  return eval(str.split('plus').join('+').split('minus').join('-')).toString();
-}
+const catchSignChange = (arr) => (!arr.length ? 0 : arr.slice(1).filter((item, index) => (Math.abs(item) > item) !== (Math.abs(arr[index]) > arr[index])).length);
 
-console.log(calculate("1plus2plus3minus4"));
 
-// "1plus2plus3plus4"  --> "10"
-// "1plus2plus3minus4" -->  "2"
+console.log(catchSignChange([1, 3, 4, 5])); // 0
+console.log(catchSignChange([1, -3, -4, 0, 5])); // 2
+console.log(catchSignChange([])); // 0
+console.log(catchSignChange([-47, 84, -30, -11, -5, 74, 77])); // 3
