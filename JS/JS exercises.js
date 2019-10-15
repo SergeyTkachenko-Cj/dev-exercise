eae6fe78a5f0c98d444aac984d799b7cbef8a02f
CJTaylor <tka4inni@gmail.com>
2019-10-15 11:09:52 +0300

    JS exercises.js (alphabetical addition - add up letters to one letter)

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,8 +1,10 @@
 
-const catchSignChange = (arr) => (!arr.length ? 0 : arr.slice(1).filter((item, index) => (Math.abs(item) > item) !== (Math.abs(arr[index]) > arr[index])).length);
+function addLetters(...letters) {
+  const cat = [...letters];
+  const res = cat.map((i) => i.charCodeAt(0) - 96);
+  const x = eval(res.join('+'));
+  const y = x % 26 || 26;
+  return String.fromCharCode(y + 96);
+}
 
-
-console.log(catchSignChange([1, 3, 4, 5])); // 0
-console.log(catchSignChange([1, -3, -4, 0, 5])); // 2
-console.log(catchSignChange([])); // 0
-console.log(catchSignChange([-47, 84, -30, -11, -5, 74, 77])); // 3
+console.log(addLetters('d', 'w', 'd', 'n', 'i', 'o', 'e'));
