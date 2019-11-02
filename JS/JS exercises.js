6015cfda1025a993127eaada516b14206d04a9ae
CJTaylor <tka4inni@gmail.com>
2019-11-02 10:39:11 +0300

    JS exercises (Sort Out The Men From Boys)
    - https://www.codewars.com/kata/sort-out-the-men-from-boys-1/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,13 +1,19 @@
-"use strict";
-exports.__esModule = true;
-function sumTriangularNumbers(n) {
-    var x = [0];
-    for (var i = 1; i <= n; i++) {
-        x.push(i + x[i - 1]);
-    }
-    return x.reduce(function (acc, cur) { return acc + cur; });
+function menFromBoys(arr) {
+    var evenArr = [];
+    var oddArr = [];
+    arr.forEach(function (i) { return i % 2 ? oddArr.push(i) : evenArr.push(i); });
+    var x = evenArr.sort(function (a, b) { return a - b; }).concat(oddArr.sort(function (a, b) { return b - a; }));
+    var count = [];
+    return x.filter(function (item) {
+        if (x.indexOf(item) === x.lastIndexOf(item)) {
+            return true;
+        }
+        else if (!count.some(function (i) { return i === item; })) {
+            count.push(item);
+            return true;
+        }
+    });
 }
-exports.sumTriangularNumbers = sumTriangularNumbers;
-console.log(sumTriangularNumbers(6)); // 56
-console.log(sumTriangularNumbers(34)); // 7140
-console.log(sumTriangularNumbers(-291)); // 0
+// console.log(menFromBoys([7, 3, 14, 17])); // [14, 17, 7, 3]
+// console.log(menFromBoys([-94, -99, -100, -99, -96, -99]));  // [-100, -96, -94, -99]
+console.log(menFromBoys([49, 818, -282, 900, 928, 281, -282, -1])); // [-282, 818, 900, 928, 281, 49, -1]
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,12 +1,21 @@
 
-export function sumTriangularNumbers(n:number) :number {
-  let x: number[] = [0];
-  for (let i = 1; i <= n; i++) {
-    x.push(i + x[i - 1]);
-  }
-  return x.reduce((acc, cur) => acc + cur)
+function menFromBoys(arr: number[]): number[] {
+  const evenArr: Array<number> = [];
+  const oddArr: Array<number> = [];
+  arr.forEach(i => i % 2 ? oddArr.push(i) : evenArr.push(i));
+  const x = evenArr.sort((a, b) => a - b).concat(oddArr.sort((a, b) => b - a));
+  let count: Array<number> = [];
+  return x.filter(item => {
+    if (x.indexOf(item) === x.lastIndexOf(item)) {
+      return true
+    }
+    else if (!count.some(i => i === item)) {
+      count.push(item);
+      return true
+    }
+  });
 }
 
-console.log(sumTriangularNumbers(6)); // 56
-console.log(sumTriangularNumbers(34));  // 7140
-console.log(sumTriangularNumbers(-291));  // 0
+// console.log(menFromBoys([7, 3, 14, 17])); // [14, 17, 7, 3]
+// console.log(menFromBoys([-94, -99, -100, -99, -96, -99]));  // [-100, -96, -94, -99]
+console.log(menFromBoys([49, 818, -282, 900, 928, 281, -282, -1])); // [-282, 818, 900, 928, 281, 49, -1]
