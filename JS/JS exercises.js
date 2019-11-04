aa46ac743ef0b852762ef3fa629780729fc8b6f3
CJTaylor <tka4inni@gmail.com>
2019-11-04 10:11:46 +0300

    JS exercises (Sum of Numbers)
    - https://www.codewars.com/kata/beginner-series-number-3-sum-of-numbers/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,19 +1,16 @@
-function menFromBoys(arr) {
-    var evenArr = [];
-    var oddArr = [];
-    arr.forEach(function (i) { return i % 2 ? oddArr.push(i) : evenArr.push(i); });
-    var x = evenArr.sort(function (a, b) { return a - b; }).concat(oddArr.sort(function (a, b) { return b - a; }));
-    var count = [];
-    return x.filter(function (item) {
-        if (x.indexOf(item) === x.lastIndexOf(item)) {
-            return true;
-        }
-        else if (!count.some(function (i) { return i === item; })) {
-            count.push(item);
-            return true;
-        }
-    });
+"use strict";
+exports.__esModule = true;
+function getSum(a, b) {
+    var strt = a < b ? a : b;
+    var fin = strt === a ? b : a;
+    var res = 0;
+    for (strt; strt <= fin; strt++) {
+        res += strt;
+    }
+    return res;
 }
-// console.log(menFromBoys([7, 3, 14, 17])); // [14, 17, 7, 3]
-// console.log(menFromBoys([-94, -99, -100, -99, -96, -99]));  // [-100, -96, -94, -99]
-console.log(menFromBoys([49, 818, -282, 900, 928, 281, -282, -1])); // [-282, 818, 900, 928, 281, 49, -1]
+exports.getSum = getSum;
+console.log(getSum(5, -1)); // 14
+console.log(getSum(0, 1)); // 0 + 1 = 1
+console.log(getSum(1, 1)); // 1 Since both are same
+console.log(getSum(-1, 2)); // -1 + 0 + 1 + 2 = 2
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,21 +1,15 @@
 
-function menFromBoys(arr: number[]): number[] {
-  const evenArr: Array<number> = [];
-  const oddArr: Array<number> = [];
-  arr.forEach(i => i % 2 ? oddArr.push(i) : evenArr.push(i));
-  const x = evenArr.sort((a, b) => a - b).concat(oddArr.sort((a, b) => b - a));
-  let count: Array<number> = [];
-  return x.filter(item => {
-    if (x.indexOf(item) === x.lastIndexOf(item)) {
-      return true
+export function getSum(a: number, b: number): number {
+let strt: number = a < b ? a : b;
+  let fin: number = strt === a ? b : a;
+  let res: number = 0;
+    for (strt; strt <= fin; strt++) {
+      res += strt;
     }
-    else if (!count.some(i => i === item)) {
-      count.push(item);
-      return true
-    }
-  });
+  return res
 }
 
-// console.log(menFromBoys([7, 3, 14, 17])); // [14, 17, 7, 3]
-// console.log(menFromBoys([-94, -99, -100, -99, -96, -99]));  // [-100, -96, -94, -99]
-console.log(menFromBoys([49, 818, -282, 900, 928, 281, -282, -1])); // [-282, 818, 900, 928, 281, 49, -1]
+console.log(getSum(5, -1)); // 14
+console.log(getSum(0, 1)); // 0 + 1 = 1
+console.log(getSum(1, 1)); // 1 Since both are same
+console.log(getSum(-1, 2)); // -1 + 0 + 1 + 2 = 2
