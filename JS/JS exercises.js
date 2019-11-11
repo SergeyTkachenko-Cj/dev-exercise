d6cb5bc892a29531489de2d70d3d8dc26419c808
CJTaylor <tka4inni@gmail.com>
2019-11-11 15:55:33 +0300

    JS exercises (You're a square!)
    - https://www.codewars.com/kata/youre-a-square/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,13 +1,10 @@
-var nbYear = function (p0, percent, aug, p) {
-    var z = 0;
-    var x = function (arg) {
-        z++;
-        p0 = arg + (arg * (percent) / 100) + aug;
-        return p0 < p ? x(p0) : p0;
-    };
-    x(p0);
-    return z;
-};
-console.log(nbYear(1500, 5, 100, 5000)); // 15
-console.log(nbYear(1500000, 2.5, 10000, 2000000)); // 10
-console.log(nbYear(1500000, 0.0, 10000, 2000000)); // 50
+"use strict";
+exports.__esModule = true;
+function isSquare(n) {
+    return Math.sqrt(n) % 1 === 0;
+}
+exports["default"] = isSquare;
+;
+console.log(isSquare(-1)); // false
+console.log(isSquare(25)); // true
+console.log(isSquare(26)); // false
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,15 +1,8 @@
 
-const nbYear = (p0, percent, aug, p) => {
-  let z:number = 0;
-    const x = (arg: any): number => {
-      z++;
-      p0 = arg + (arg * (percent)/100) + aug;
-      return p0 < p ? x(p0) : p0
-    }
-    x(p0);
-  return z
-}
+export default function isSquare(n: number): boolean {
+  return Math.sqrt(n) % 1 === 0
+};
 
-console.log(nbYear(1500, 5, 100, 5000)); // 15
-console.log(nbYear(1500000, 2.5, 10000, 2000000)); // 10
-console.log(nbYear(1500000, 0.0, 10000, 2000000)); // 50
+console.log(isSquare(-1));  // false
+console.log(isSquare(25));  // true
+console.log(isSquare(26));  // false
