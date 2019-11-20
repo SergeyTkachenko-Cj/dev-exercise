5630db9d3046ee1f3f2a802ed0a2166443cd5c00
CJTaylor <tka4inni@gmail.com>
2019-11-20 13:25:45 +0300

    JS exercises (Roman Numerals Decoder)
    - https://www.codewars.com/kata/51b6249c4612257ac0000005/solutions/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,6 +1,23 @@
 "use strict";
 exports.__esModule = true;
-exports.factorial = function (n) {
-    return n > 1 ? n * exports.factorial(n - 1) : 1;
-};
-console.log(exports.factorial(5)); // 120
+function solution(roman) {
+    var decode = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1
+    };
+    return roman.split('')
+        .reverse()
+        .map(function (i) { return decode[i]; })
+        .reduce(function (acc, cur, indx, arr) {
+        return arr[indx - 1] > cur ? acc - cur : acc + cur;
+    });
+}
+exports.solution = solution;
+// console.log(solution('XXI')); // 21
+// console.log(solution('IV')); // 4
+console.log(solution('MCMXC')); // 1990
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,6 +1,24 @@
 
-export const factorial = (n: number): number => {
-  return n > 1 ? n * factorial(n - 1) : 1
-};
+export function solution(roman: string): number {
 
-console.log(factorial(5));  // 120
+  const decode: any = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1
+  }
+
+  return roman.split('') 
+              .reverse()  
+              .map(i => decode[i])
+              .reduce((acc, cur, indx, arr) => {
+                return arr[indx - 1] > cur ? acc - cur : acc + cur
+              });
+}
+
+// console.log(solution('XXI')); // 21
+// console.log(solution('IV')); // 4
+console.log(solution('MCMXC')); // 1990
