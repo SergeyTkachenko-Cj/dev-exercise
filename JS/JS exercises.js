bf0ccccfc74c04cd0211fcd4dbc4e885851e3241
CJTaylor <tka4inni@gmail.com>
2019-12-11 10:59:54 +0300

    JS exercises (Ease the StockBroker) - Ugly version
    - https://www.codewars.com/kata/ease-the-stockbroker/train/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,15 +1,40 @@
 "use strict";
 exports.__esModule = true;
-function growingPlant(up, down, h) {
-    var res = 0;
-    var days = 0;
-    for (var i = 0; res < h; i++) {
-        i % 2 ? res -= down : res += up;
-        days++;
+var G964 = /** @class */ (function () {
+    function G964() {
     }
-    return Math.ceil(days / 2) || 1;
-}
-exports.growingPlant = growingPlant;
-console.log(growingPlant(100, 10, 910)); // 10
-console.log(growingPlant(10, 9, 4)); // 1
-console.log(growingPlant(43, 13, 959)); // 32
+    G964.balanceStatements = function (list) {
+        var regex = /([A-Z])+\s\d+\s(\d+)?\.\d+\s(B|S)/;
+        var arr = list.split(',');
+        var bad = 0;
+        var badStr = '';
+        var sumBuy = 0;
+        var sumSell = 0;
+        var arrFilt = arr.filter(function (i) {
+            if (regex.test(i)) {
+                /\sB/.test(i) ? sumBuy += +(i.match(/(\d+)?\.\d+/) || [])[0] * +(i.match(/\s\d+/) || []) :
+                    sumSell += +(i.match(/(\d+)?\.\d+/) || [])[0] * +(i.match(/\s\d+/) || []);
+                return i.match(regex);
+            }
+            else {
+                var noSpace = i.replace(/^\s/, '');
+                if (!badStr.length)
+                    badStr += ' ' + noSpace + ' ;';
+                else {
+                    badStr += noSpace + ' ;';
+                }
+                bad++;
+                return false;
+            }
+        });
+        var badly = "; Badly formed " + bad + ":" + badStr;
+        arr[0].length ? bad : bad = 0;
+        return "Buy: " + Math.round(sumBuy) + " Sell: " + Math.round(sumSell) + (bad ? badly : '');
+    };
+    return G964;
+}());
+exports.G964 = G964;
+console.log(G964.balanceStatements("GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"));
+//  "Buy: 169850 Sell: 116000; Badly formed 1: CSCO 250.0 29 B ;"
+// console.log(G964.balanceStatements("ZNGA 1300 2.66, CLH15.NYM 50 56.32 S, OWW 1000 11.623 S, OGG 20 580.1 S"));
+//  "Buy: 0 Sell: 26041; Badly formed 1: ZNGA 1300 2.66 ;"
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,17 +1,39 @@
 
-export function growingPlant(up: number, down: number, h: number): number {
+export class G964 {
 
-  let res: number = 0;
-  let days: number = 0;
+  public static balanceStatements = (list: string) => {
+    const regex = /([A-Z])+\s\d+\s(\d+)?\.\d+\s(B|S)/;
 
-  for (let i = 0; res < h; i++) {
-    i % 2 ? res -= down : res += up;
-      days++;
-    }
+    const arr: Array<string> = list.split(',');
+    let bad: number = 0;
+    let badStr: string = '';
+    let sumBuy: any = 0;
+    let sumSell: any = 0;
+    
+    const arrFilt = arr.filter(i => {
+      if (regex.test(i)) {
+        /\sB/.test(i) ? sumBuy += +(i.match(/(\d+)?\.\d+/) || [])[0] * +(i.match(/\s\d+/) || []) : 
+                        sumSell += +(i.match(/(\d+)?\.\d+/) || [])[0] * +(i.match(/\s\d+/) || []);
+        return i.match(regex)
+      }
+      else {
+        const noSpace: string = i.replace(/^\s/, '');
+        if (!badStr.length) badStr += ' '+ noSpace + ' ;';
+        else { badStr += noSpace + ' ;' }
+        bad++;
+        return false
+      } 
+    })
 
-  return Math.ceil(days / 2) || 1
+    let badly: string = `; Badly formed ${bad}:${badStr}`;
+
+    arr[0].length ? bad : bad = 0;
+
+    return `Buy: ${Math.round(sumBuy)} Sell: ${Math.round(sumSell)}${bad ? badly : ''}`
+  }
 }
 
-console.log(growingPlant(100, 10, 910));  // 10
-console.log(growingPlant(10, 9, 4));  // 1
-console.log(growingPlant(43, 13, 959)); // 32
+console.log(G964.balanceStatements("GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"));  
+//  "Buy: 169850 Sell: 116000; Badly formed 1: CSCO 250.0 29 B ;"
+// console.log(G964.balanceStatements("ZNGA 1300 2.66, CLH15.NYM 50 56.32 S, OWW 1000 11.623 S, OGG 20 580.1 S"));
+//  "Buy: 0 Sell: 26041; Badly formed 1: ZNGA 1300 2.66 ;"
