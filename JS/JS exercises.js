ae292aba69b2788ef419b67dec72fd4a4849ec91
CJTaylor <tka4inni@gmail.com>
2019-12-19 12:50:51 +0300

    JS exercises (Help the bookseller)
    - https://www.codewars.com/kata/help-the-bookseller/train/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -3,36 +3,27 @@ exports.__esModule = true;
 var G964 = /** @class */ (function () {
     function G964() {
     }
-    G964.validation = function (item) {
-        var arr = item.split(' ');
-        return arr.length == 4 &&
-            /^\d+$/.test(arr[1]) &&
-            /\./.test(arr[2]) &&
-            /B|S/.test(arr[3]) &&
-            { status: arr[3], sum: +arr[1] * +arr[2] };
-    };
-    G964.balanceStatements = function (list) {
-        var arr = list.split(/, /);
-        var bad = [];
-        var buy = 0;
-        var sell = 0;
-        arr.forEach(function (i) {
-            var itemValid = G964.validation(i);
-            if (itemValid) {
-                itemValid.status == 'B' ? buy += itemValid.sum : sell += itemValid.sum;
-            }
-            else {
-                if (i)
-                    bad.push(i);
-            }
+    G964.stockList = function (listOfArt, listOfCat) {
+        var listStr = listOfArt.join(',');
+        var resArr = listOfCat.map(function (i) {
+            var re = new RegExp("\\b(" + i + ")\\w+\\s(\\d+)", "g");
+            var numb = 0;
+            var execute = function () {
+                var reCheck = re.exec(listStr);
+                if (reCheck) {
+                    numb += +reCheck[2];
+                    execute();
+                }
+            };
+            execute();
+            return "(" + i + " : " + numb + ")";
         });
-        return "Buy: " + Math.round(buy) + " Sell: " + Math.round(sell)
-            + (bad.length ? "; Badly formed " + bad.length + ": " + bad.join(' ;') + " ;" : "");
+        return !listStr.length ? '' : resArr.join(' - ');
     };
     return G964;
 }());
 exports.G964 = G964;
-console.log(G964.balanceStatements("GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"));
-//  "Buy: 169850 Sell: 116000; Badly formed 1: CSCO 250.0 29 B ;"
-// console.log(G964.balanceStatements("ZNGA 1300 2.66, CLH15.NYM 50 56.32 S, OWW 1000 11.623 S, OGG 20 580.1 S"));
-//  "Buy: 0 Sell: 26041; Badly formed 1: ZNGA 1300 2.66 ;"
+console.log(G964.stockList(["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"], ["A", "B", "C", "D"]));
+//  "(A : 0) - (B : 1290) - (C : 515) - (D : 600)"
+// console.log(G964.stockList([], ["A", "B", "C", "D"]));
+//  ''
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,39 +1,30 @@
 
 export class G964 {
 
-  public static validation = (item: string) => {
-    const arr: Array<string> = item.split(' ');
+  public static stockList = (listOfArt: Array<string>, listOfCat: Array<string>): string => {
+      const listStr: string = listOfArt.join(',');
 
-    return arr.length == 4 &&
-           /^\d+$/.test(arr[1]) &&
-           /\./.test(arr[2]) &&
-           /B|S/.test(arr[3]) &&
-           {status: arr[3], sum: +arr[1] * +arr[2]}
-  }
+      const resArr = listOfCat.map(i => {
+        const re = new RegExp("\\b(" + i +")\\w+\\s(\\d+)", "g");
+        let numb: number = 0;
 
-    public static balanceStatements = (list) => {
-        const arr: Array<string> = list.split(/, /);
-      let bad: Array<string> = [];
-      let buy: number = 0;
-      let sell: number = 0;
-
-      arr.forEach(i => {
-        const itemValid = G964.validation(i);
-          if (itemValid) {
-            itemValid.status == 'B' ? buy += itemValid.sum : sell += itemValid.sum;
+          const execute = () => {
+            const reCheck = re.exec(listStr);
+              if (reCheck) {
+                numb += +reCheck[2];
+                execute();
+              }
           }
-          else {
-            if (i) bad.push(i);
-          }
+
+        execute();
+        return `(${i} : ${numb})`
       });
-      
-      return `Buy: ${Math.round(buy)} Sell: ${Math.round(sell)}` 
-      + (bad.length ? `; Badly formed ${bad.length}: ${bad.join(' ;')} ;` : "")
-  
-    }
+
+      return !listStr.length ? '' : resArr.join(' - ')
+  }
 }
 
-console.log(G964.balanceStatements("GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"));  
-//  "Buy: 169850 Sell: 116000; Badly formed 1: CSCO 250.0 29 B ;"
-// console.log(G964.balanceStatements("ZNGA 1300 2.66, CLH15.NYM 50 56.32 S, OWW 1000 11.623 S, OGG 20 580.1 S"));
-//  "Buy: 0 Sell: 26041; Badly formed 1: ZNGA 1300 2.66 ;"
+console.log(G964.stockList(["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"], ["A", "B", "C", "D"]));
+//  "(A : 0) - (B : 1290) - (C : 515) - (D : 600)"
+// console.log(G964.stockList([], ["A", "B", "C", "D"]));
+//  ''
