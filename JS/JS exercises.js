9ba16fd4108f759be107387be1e2047712c2fc59
CJTaylor <tka4inni@gmail.com>
2019-12-25 13:40:52 +0300

    JS exercises (Take a Ten Minute Walk)
    - https://www.codewars.com/kata/54da539698b8a2ad76000228/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,9 +1,14 @@
 "use strict";
 exports.__esModule = true;
-function comp(a1, a2) {
-    return a1 && a2 ? a1.map(function (i) { return i * i; }).sort().toString() === a2.sort().toString() : false;
+function isValidWalk(walk) {
+    var comp = function (arg) {
+        return arg.map(function (i) { return (walk.toString().match(i) || '').length; })
+            .reduce(function (acc, cur) { return acc - cur ? 0 : acc + cur; });
+    };
+    return comp([/n/g, /s/g]) + comp([/e/g, /w/g]) === 10;
 }
-exports.comp = comp;
-console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]));
-console.log(comp(null, null));
-console.log(comp([], []));
+exports.isValidWalk = isValidWalk;
+console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])); // true
+// console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])); // false
+// console.log(isValidWalk(['w'])); // false
+// console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']));  // false
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,9 +1,13 @@
 
-export function comp(a1: number[] | null, a2: number[] | null): boolean {
-
-  return a1 && a2 ? a1.map(i => i * i).sort().toString() === a2.sort().toString() : false
+export function isValidWalk(walk: string[]) {
+  const comp = (arg: Array<RegExp>): number => {
+    return arg.map(i => (walk.toString().match(i) || '').length)
+              .reduce((acc, cur) => acc - cur ? 0 : acc + cur) 
+  }
+  return comp([/n/g, /s/g]) + comp([/e/g, /w/g]) === 10
 }
 
-console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]));
-console.log(comp(null, null));
-console.log(comp([],[]));
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])); // true
+// console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])); // false
+// console.log(isValidWalk(['w'])); // false
+// console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']));  // false
