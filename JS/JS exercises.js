c5f2451e59697e1c439fdffb667b2701c8e9199c
CJTaylor <tka4inni@gmail.com>
2020-02-12 14:44:57 +0300

    JS exercises (Regexp Basics - is it an eight bit unsigned number?) - No regex version
    - https://www.codewars.com/kata/567e8f7b4096f2b4b1000005/solutions/typescript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,44 +1,11 @@
 "use strict";
 exports.__esModule = true;
-function getW(height) {
-    var arr = [];
-    var str = '*';
-    var empt_str = ' ';
-    var space = (height + (height - 1)) - 2;
-    if (space < 0)
-        return '';
-    for (var i = 0; i < height; i++) {
-        switch (i) {
-            case 0:
-                arr.push("" + empt_str.repeat(i) + str + (empt_str.repeat(space) + str).repeat(2) + empt_str.repeat(i));
-                break;
-            case height - 1:
-                arr.push("" + empt_str.repeat(i) + str + (empt_str.repeat(space) + str).repeat(1) + empt_str.repeat(i));
-                break;
-            default:
-                arr.push("" + (empt_str.repeat(i) + str + empt_str.repeat(space) + str + empt_str.repeat(i + (i - 1)) + str + empt_str.repeat(space) + str) + empt_str.repeat(i));
-        }
-        space = space > 1 ? space - 2 : (height + (height - 1)) - 2;
-    }
-    return arr.join('\n');
-}
-exports.getW = getW;
-console.log(getW(5));
-//      2
-// [
-//   "* * *", 
-//   " * * "
-// ]
-//      3
-// [
-//   "*   *   *", 
-//   " * * * * ", 
-//   "  *   *  "
-// ]
-//      4
-// [
-//   "*     *     *", 
-//   " *   * *   * ", 
-//   "  * *   * *  ",
-//   "   *     *   "
-// ]
+String.prototype.eightBitNumber = function () {
+    return +this >= 0 &&
+        +this <= 255 &&
+        this.length === (+this).toString().length;
+};
+console.log("".eightBitNumber()); // false
+console.log("0".eightBitNumber()); // true
+console.log("00".eightBitNumber()); // false
+console.log("197".eightBitNumber()); // true
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,48 +1,18 @@
-export function getW(height: number): string {
-  
-  const arr: Array<string> = [];
-  const str: string = '*';
-  const empt_str: string = ' ';
-  let space: number = (height + (height - 1)) - 2;
+export {};
 
-  if (space < 0) return ''
-
-    for (let i = 0; i < height; i++) {
-      switch (i) {
-        case 0:
-          arr.push(`${empt_str.repeat(i)}${str}${(empt_str.repeat(space) + str).repeat(2)}${empt_str.repeat(i)}`);
-          break;
-        case height - 1:
-          arr.push(`${empt_str.repeat(i)}${str}${(empt_str.repeat(space) + str).repeat(1)}${empt_str.repeat(i)}`);
-          break;
-        default:
-          arr.push(`${empt_str.repeat(i) + str + empt_str.repeat(space) + str + empt_str.repeat(i + (i - 1)) + str + empt_str.repeat(space) + str}${empt_str.repeat(i)}`);
-      }
-      space = space > 1 ? space - 2 : (height + (height - 1)) - 2;
-    }
-
-  return arr.join('\n')
+declare global {
+  interface String {
+    eightBitNumber(): boolean;
+  }
 }
 
-console.log(getW(5));
+String.prototype.eightBitNumber = function(): boolean {
+  return +this >= 0 && 
+         +this <= 255 && 
+         this.length === (+this).toString().length
+}
 
-//      2
-// [
-//   "* * *", 
-//   " * * "
-// ]
-
-//      3
-// [
-//   "*   *   *", 
-//   " * * * * ", 
-//   "  *   *  "
-// ]
-
-//      4
-// [
-//   "*     *     *", 
-//   " *   * *   * ", 
-//   "  * *   * *  ",
-//   "   *     *   "
-// ]
+console.log("".eightBitNumber());   // false
+console.log("0".eightBitNumber());  // true
+console.log("00".eightBitNumber());  // false
+console.log("197".eightBitNumber());  // true
