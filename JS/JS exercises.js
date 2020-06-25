0e714d30c6f11f5f9e9440aefbb55f5b2884063a
CJTaylor <tka4inni@gmail.com>
2020-06-25 12:56:03 +0300

    JS exercises (Quarter of the year)
    - https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/solutions/javascript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,8 +1,7 @@
-"use strict";
-exports.__esModule = true;
-function noOdds(values) {
-    return values.filter(function (i) { return !(i % 2); });
+const quarterOf = month => {
+    console.log(Math.ceil(month / 3));
 }
-exports.noOdds = noOdds;
-console.log(noOdds([0, 1])); // [0]
-console.log(noOdds([0, 1, 2, 3])); // [0,2]
+
+quarterOf(3);
+quarterOf(8);
+quarterOf(11);
--- JS/JS exercises.ts
+++ JS/JS exercises.ts
@@ -1,6 +1,14 @@
-export function noOdds(values: number[]): number[] {
-  return values.filter(i => !(i % 2))
+export function anchorize(text:string): string {
+  const replacer = (mtch: string): string => {
+    console.log('match: '+ mtch);
+    return ` <a href="${mtch.replace(/\s/g, '')}">${mtch.replace(/\s/g, '')}</a>`
+  }
+  return text.replace(/\s([\w|:|\/|\.])+/g, replacer)
 }
 
-console.log(noOdds([0,1])); // [0]
-console.log(noOdds([0,1,2,3])); // [0,2]
+// console.log(anchorize('hello FTP://world.com !')); 
+// 'hello <a href="FTP://world.com">FTP://world.com</a> !' 
+console.log(anchorize('hello http://world.com !')); 
+// 'hello <a href="http://world.com">http://world.com</a> !' 
+
+// console.log(anchorize('hello file://C:/world.txt !')); 
