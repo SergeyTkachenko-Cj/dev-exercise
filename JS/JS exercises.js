0ae6d137559d7b750905ebdb7c4791f5d2ac84c3
CJTaylor <tka4inni@gmail.com>
2020-06-25 14:15:58 +0300

    JS exercises (Total amount of points)
    - https://www.codewars.com/kata/5bb904724c47249b10000131/javascript

--- JS/JS exercises.js
+++ JS/JS exercises.js
@@ -1,7 +1,11 @@
-function dutyFree(normPrice, discount, hol) {
-    return Math.floor(hol / (normPrice / (100 / discount)))
+function points(games) {
+    return games.reduce((acc, cur) => {
+        return acc += cur[0] > cur[2] ? 3 : cur[0] === cur[2] ? 1 : 0
+    }, 0)
 }
 
-console.log(dutyFree(12, 50, 1000));
-console.log(dutyFree(17, 10, 500));
-console.log(dutyFree(24, 35, 3000));
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"])); // 30
+console.log(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"])); // 10
+console.log(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"])); // 0
+console.log(points(["1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"])); // 15
+console.log(points(["1:0","2:0","3:0","4:4","2:2","3:3","1:4","2:3","2:4","3:4"])); // 12
